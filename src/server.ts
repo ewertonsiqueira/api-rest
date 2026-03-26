708fb4e51b93b09831085bd5aca7d37e6bea542e
Ewerton Siqueira <ewertoncostaxx@hotmail.com>
2026-03-25 22:16:02 -0300

    add middleware for request logging

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import { myMiddleware } from './middlewares/middleware';
 
 const app = express();
 const PORT = 3333;
 
+app.use(express.json());
+app.use(myMiddleware);
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.post('/products', (req, res) => {
+  console.log('Received product data:', req.body);
+  const { name, price, description } = req.body;
+  // res.send(`Product created: ${name} - $${price} - ${description}`);
+  res.status(201).json({ message: 'Produto criado com sucesso', product: { name, price, description } });
 });
 
 app.listen(PORT, () => {
